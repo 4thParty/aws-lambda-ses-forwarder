0fe58312fcfa9c39b1eec06029100fd44fcf11d5
Gary Chapman <garychapman@iprimus.com.au>
2021-03-11 16:54:05 +1100

    GE-2061 - user lookup now based on Forwarder Match Address or bhid

--- AirTable.js
+++ AirTable.js
@@ -4,14 +4,15 @@ const Slack = require('./Slack');
 const AirtableBase = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
 
 module.exports = {
-    lookupUser: async (bhid) => {
+    lookupUser: async (userName) => {
+        var userNameLower = userName.toLowerCase();
         return AirtableBase(process.env.AIRTABLE_USERS_TABLE).select(
-            { filterByFormula: `LOWER(bhid)="${bhid.toLowerCase()}"` }
+            { filterByFormula: `OR(SEARCH('${userNameLower}', LOWER({Forwarder match address})), LOWER(bhid)='${userNameLower}'))` }
         )
         .all()
         .then((recs) => {
             if (!recs || !recs.length) {
-                var msg = `AirTable lookup for bhid '${bhid}' found 0 records`;
+                var msg = `AirTable lookup for userName '${userName}' found 0 records`;
                 console.log(msg);
                 if (process.env.SLACK_CHANNEL) {
                     return Slack.sendMessage(msg, ':mag:')
@@ -20,12 +21,12 @@ module.exports = {
             }
 
             var email = recs[0].fields["Email"];
-            console.log(`AirTable lookup for '${bhid}' found ${email}`);
+            console.log(`AirTable lookup for '${userName}' found ${email}`);
             
             return email;
         })
         .catch((err) => {
-            var msg = `AirTable lookup for bhid '${bhid}' failed: ${err}`;
+            var msg = `AirTable lookup for userName '${userName}' failed: ${err}`;
             console.log(msg);
             
             if (process.env.SLACK_CHANNEL) {
